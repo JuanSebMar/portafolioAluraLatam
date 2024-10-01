7f59b60933f75d5ab163b5dd8d41cf39a25371ed
Sebastian Soto <juansebastiansotomartin@gmail.com>
2024-10-01 16:21:40 -0300

    finalizacion de estilos responsive

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ document
   .querySelectorAll('.navegacion-lista a[href^="#"]')
   .forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
-      e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+      e.preventDefault();
 
-      // Obtiene el elemento de destino a partir del href
       const target = document.querySelector(this.getAttribute("href"));
 
-      // Realiza el desplazamiento suave hacia el elemento objetivo
       target.scrollIntoView({
-        behavior: "smooth", // Scroll suave
-        block: "start", // Alinea al inicio de la página
+        behavior: "smooth",
+        block: "start",
       });
     });
   });
